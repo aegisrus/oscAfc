7ce1a604f88e0328de786cc60d5c44a9e947bd81
moter1026 <matvej.sidoroff2016@yandex.ru>
2026-02-18 22:20:58 +0400

    first commit

---
+++ seeOSC.py
@@ -0,0 +1,398 @@
+# Версия SeeOsc без открытия ald файла
+
+import inspect
+import os
+import sys
+
+os.environ['PYQTGRAPH_QT_LIB'] = 'PyQt5' # Устанавливаю переменную окружения для pyqtgraph
+
+import numpy as np
+import pyqtgraph as pg
+
+from numba import njit
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt, pyqtSignal, QSize
+from PyQt5.QtWidgets import (QWidget, QPushButton,
+                             QLabel, QVBoxLayout,
+                             QHBoxLayout, QMessageBox,
+                             QMainWindow, QFileDialog,
+                             QLayout)
+from PyQt5.QtGui import QFontDatabase, QFont
+
+import Aegis_osc
+
+from Fourier import Fourier, four2
+from work_with_osc import DataOsc, get_dB_osc
+
+
+LOG_LEVEL = Aegis_osc.LogLevel
+
+@njit
+def set_K_mkV_and_dB(end_data_osc: int, K_mkV: np.ndarray, osc_datas: np.ndarray) -> np.ndarray:
+    """Метод рассчитывает Децибелы для осциллограммы"""
+    dB_data = np.zeros(end_data_osc, dtype=np.int32)  # Массив с нужной длиной
+    for i in range(end_data_osc):
+        dB_data[i] = np.int32(get_dB_osc(osc_datas[i], K_mkV[i]))
+    return dB_data
+
+class MainMenu(QWidget):
+    # Создаем сигнал, который уведомит об отображении графиков
+    graphs_shown = pyqtSignal()
+    osc_now_changed = pyqtSignal(object)
+
+    def __init__(self):
+        super().__init__()  # Вызываю конструктор родительского класса QWidget
+        self.__set_style_for_app()
+        self.logger = Aegis_osc.Logger("log_seeOSC.txt")
+
+        self.dB_text = None
+        self.text = None
+        self.now_plot_spectr_item = None
+        self.now_plot_osc_item = None
+        self.main_layout_v = QVBoxLayout(self)
+        self.main_layout_v.setSizeConstraint(QLayout.SetMinimumSize)
+
+        # Кнопка для получения открытия отдельной осциллограммы
+        self.startWidget = QWidget(self)
+        self.startVLayout = QVBoxLayout(self)
+        self.bttn_open_alone_osc = QPushButton()
+        self.bttn_open_alone_osc.setText("открыть osc")
+        self.bttn_open_alone_osc.clicked.connect(self._on_clicked_bttn_open_osc)
+        self.startVLayout.addWidget(self.bttn_open_alone_osc)
+        self.startWidget.setLayout(self.startVLayout)
+        self.startWidget.setMinimumSize(300, 150)
+
+        self.file_open = QLabel(self)
+        self.info_now_num_osc = QLabel(self)
+
+        # Блок добавления элементов в главное меню
+        self.main_layout_v.addWidget(self.startWidget)
+
+    def _on_clicked_bttn_open_osc(self) -> None:
+        """Обработчик события клика для кнопки создания нового датасета"""
+        path, _ = QFileDialog.getOpenFileName(self, "Выберите файл", "", "osc files (*.osc)")
+
+        self.__open_osc(path)
+
+    def __open_osc(self, name_osc: str):
+        try:
+            self.startWidget.setMinimumSize(0, 0)  # Сбрасываем ограничения на размер виджета
+
+            self.name_osc = name_osc
+
+            self.logger.logg(LOG_LEVEL._INFO_, f"Открытие файла .osc: {self.name_osc}",
+                        os.path.basename(__file__),
+                        inspect.currentframe().f_lineno, 
+                        inspect.currentframe().f_code.co_name)
+            self.osc_file = Aegis_osc.File_osc(self.name_osc, self.logger)
+
+            self.file_open.setText(f"Открыт файл .osc: {self.name_osc}")
+            self.num_osc = self.osc_file.sdoHdr.NumOSC
+
+            # Предварительная инициализация массивов
+            self.start_data_osc = 0
+            self.end_data_osc = min(self.num_osc, 500)
+
+            # Получаем данные и инициализируем массивы заранее
+            self.logger.logg(LOG_LEVEL._INFO_, f"Получение данных из файла .osc",
+                        os.path.basename(__file__),
+                        inspect.currentframe().f_lineno, 
+                        inspect.currentframe().f_code.co_name)
+            self.osc_datas = np.array(self.osc_file.getDotsOSC(0, self.end_data_osc))  
+                      
+            # Заполняем массивы за один проход, без использования np.append
+            self.logger.logg(LOG_LEVEL._INFO_, f"Старт получения K_mkV",
+                        os.path.basename(__file__),
+                        inspect.currentframe().f_lineno, 
+                        inspect.currentframe().f_code.co_name)
+            self.K_mkV = np.array(self.osc_file.get_K_mkV(0, self.end_data_osc))
+            self.logger.logg(LOG_LEVEL._INFO_, f"Конец получения K_mkV",
+                        os.path.basename(__file__),
+                        inspect.currentframe().f_lineno, 
+                        inspect.currentframe().f_code.co_name)
+            self.logger.logg(LOG_LEVEL._INFO_, f"Старт получения децибел",
+                    os.path.basename(__file__),
+                    inspect.currentframe().f_lineno, 
+                    inspect.currentframe().f_code.co_name)
+            self.dB_data = set_K_mkV_and_dB(self.end_data_osc, self.K_mkV, self.osc_datas)
+            self.logger.logg(LOG_LEVEL._INFO_, f"Конец получения децибел",
+                        os.path.basename(__file__),
+                        inspect.currentframe().f_lineno, 
+                        inspect.currentframe().f_code.co_name)
+            self.plot_layout_h = QHBoxLayout(self)
+            self.osc_now = 0
+            if not hasattr(self, "now_plot_osc"):
+                self.logger.logg(LOG_LEVEL._INFO_, f"Создание графиков и их отображение",
+                        os.path.basename(__file__),
+                        inspect.currentframe().f_lineno, 
+                        inspect.currentframe().f_code.co_name)
+                self.now_plot_osc = pg.PlotWidget(parent=self)
+                self.now_plot_spectr = pg.PlotWidget(parent=self)
+
+                self.now_plot_osc.setTitle("Осциллограмма")
+                self.now_plot_spectr.setTitle("Спектр осциллограммы")
+                self.now_plot_osc.showGrid(x=True, y=True)
+                self.now_plot_spectr.showGrid(x=True, y=True)
+
+                self.plot_layout_h.addWidget(self.now_plot_osc)
+                self.plot_layout_h.addWidget(self.now_plot_spectr)
+                self.now_plot_osc.setMinimumSize(QSize(600, 250))
+                self.now_plot_osc.setMaximumHeight(400)
+                self.now_plot_spectr.setMinimumSize(QSize(600, 250))
+                self.now_plot_spectr.setMaximumHeight(400)
+
+                self.main_layout_v.addWidget(self.file_open)
+                self.main_layout_v.addWidget(self.info_now_num_osc)
+
+                self.main_layout_v.addLayout(self.plot_layout_h)
+
+            self.now_plot_osc.clear()
+            self.now_plot_spectr.clear()
+
+            self.info_now_num_osc.setText(f"Номер кадра: {self.osc_now + 1} из {self.num_osc}")
+            self.now_plot_osc_item = self.now_plot_osc.plot(self.osc_datas[self.osc_now])
+
+            self.logger.logg(LOG_LEVEL._INFO_, f"Построение спектра осциллограммы",
+                        os.path.basename(__file__),
+                        inspect.currentframe().f_lineno, 
+                        inspect.currentframe().f_code.co_name)
+            spectr = four2(np.array(self.osc_datas[self.osc_now], dtype=np.float64))
+            x = np.array([i / (len(spectr) / 2 / 500) for i in range(round(len(spectr) / 2))])
+            self.now_plot_spectr_item = self.now_plot_spectr.plot(x, spectr[:round(len(spectr) / 2)])
+            # Добавляем информацию о том, какая частота более выражена в сигнале
+            index = np.argmax(spectr[:round(len(spectr) / 2)])
+            self.text = pg.TextItem(f"{x[index]} кГц", anchor=(0, 1), color="r")
+            self.now_plot_spectr.addItem(self.text)
+
+            # Связываем изменение диапазона графика с обновлением позиции текста
+            self.now_plot_spectr.sigRangeChanged.connect(self.__update_info_position)
+
+            # Добавляем информацию о децибелах
+            self.dB_text = pg.TextItem(f"{self.dB_data[self.osc_now]} Дб", anchor=(0, 1), color="r")
+            self.now_plot_osc.addItem(self.dB_text)
+
+            self.__update_info_position()
+
+            # Связываем изменение диапазона графика с обновлением позиции текста
+            self.now_plot_osc.sigRangeChanged.connect(self.__update_info_position)
+
+            # self.plot_layout_h.addWidget(pg.plot(self.osc_datas[self.osc_now]))
+            if (not hasattr(self, "bttn_next_osc") and
+                    not hasattr(self, "bttn_prev_osc")):
+                self.logger.logg(LOG_LEVEL._INFO_, f"Создание кнопок для переключения между осциллограммами",
+                        os.path.basename(__file__),
+                        inspect.currentframe().f_lineno, 
+                        inspect.currentframe().f_code.co_name)
+                self.bttn_next_osc = QPushButton("Следующая осциллограмма")
+                self.bttn_prev_osc = QPushButton("Предыдущая осциллограмма")
+                self.bttn_next_osc.clicked.connect(self.open_next_osc)
+                self.bttn_prev_osc.clicked.connect(self.open_prev_osc)
+                self.main_layout_v.addWidget(self.bttn_next_osc)
+                self.main_layout_v.addWidget(self.bttn_prev_osc)
+            self.check_next_prev_osc()
+            # Отправляем сигнал о том, что графики были отображены
+            self.adjustSize()
+            self.graphs_shown.emit()
+        except Exception as e:
+            self.logger.logg(LOG_LEVEL._CRITICAL_, f"Ошибка при открытии файла OSC: {e}",
+                        os.path.basename(__file__),
+                        inspect.currentframe().f_lineno, 
+                        inspect.currentframe().f_code.co_name)
+
+    def __update_info_position(self) -> None:
+        """Обновляет позицию текстового элемента в правом верхнем углу графика."""
+        graph = self.sender()
+
+        if graph == self.now_plot_osc:
+            y_min, y_max = self.now_plot_osc_item.dataBounds(1)
+            x_min, x_max = self.now_plot_osc_item.dataBounds(0)
+            x_width = x_max - x_min
+            y_height = y_max - y_min
+            delta_x = x_width * 0.85
+            delta_y = y_height * 0.9
+            # Устанавливаем текст в правый верхний угол с небольшим отступом
+            self.dB_text.setPos(x_min + delta_x, y_min + delta_y)
+            return
+        elif graph == self.now_plot_spectr:
+            y_min, y_max = self.now_plot_spectr_item.dataBounds(1)
+            x_min, x_max = self.now_plot_spectr_item.dataBounds(0)
+            x_width = x_max - x_min
+            y_height = y_max - y_min
+            delta_x = x_width * 0.85
+            delta_y = y_height * 0.9
+            # Устанавливаем текст в правый верхний угол с небольшим отступом
+            self.text.setPos(x_min + delta_x, y_min + delta_y)
+            return
+        else:
+            # Если метод вызван напрямую, то устанавливаем инфо-текст в верхний правый угол у обоих графиков
+            # для self.now_plot_osc
+            y_min, y_max = self.now_plot_osc_item.dataBounds(1)
+            x_min, x_max = self.now_plot_osc_item.dataBounds(0)
+            x_width = x_max - x_min
+            y_height = y_max - y_min
+            delta_x = x_width * 0.85
+            delta_y = y_height * 0.9
+            # Устанавливаем текст в правый верхний угол с небольшим отступом
+            self.dB_text.setPos(x_min + delta_x, y_min + delta_y)
+
+            # для self.now_plot_spectr
+            y_min, y_max = self.now_plot_spectr_item.dataBounds(1)
+            x_min, x_max = self.now_plot_spectr_item.dataBounds(0)
+            x_width = x_max - x_min
+            y_height = y_max - y_min
+            delta_x = x_width * 0.85
+            delta_y = y_height * 0.9
+            # Устанавливаем текст в правый верхний угол с небольшим отступом
+            self.text.setPos(x_min + delta_x, y_min + delta_y)
+        return
+
+    def __load_prev_osc(self):
+        if self.osc_now <= 0:
+            return
+        self.end_data_osc = self.start_data_osc
+        self.start_data_osc = self.start_data_osc - 500 if (self.start_data_osc - 500) > 0 else 0
+        self.osc_datas = np.array(self.osc_file.getDotsOSC(self.start_data_osc, self.end_data_osc))
+
+        self.K_mkV = np.array(self.osc_file.get_K_mkV(self.start_data_osc, self.end_data_osc))
+        self.dB_data = np.array(([get_dB_osc(self.osc_datas[i], self.K_mkV[i])
+                                  for i in range(0, self.end_data_osc - self.start_data_osc)]))
+
+    def __load_next_osc(self):
+        if self.osc_now >= self.num_osc - 1:
+            return
+        self.start_data_osc = self.end_data_osc
+        self.end_data_osc = self.end_data_osc + 500 if (self.num_osc - self.osc_now) > 500 else self.num_osc
+        self.osc_datas = np.array(self.osc_file.getDotsOSC(self.start_data_osc, self.end_data_osc))
+
+        self.K_mkV = np.array(self.osc_file.get_K_mkV(self.start_data_osc, self.end_data_osc))
+        self.dB_data = np.array(([get_dB_osc(self.osc_datas[i], self.K_mkV[i])
+                                  for i in range(0, self.end_data_osc - self.start_data_osc)]))
+
+    def keyPressEvent(self, event):
+        """
+        Переопределение метода обработки событий нажатия клавиш
+        """
+        if event.key() == Qt.Key_Left:
+            self.open_prev_osc()
+        elif event.key() == Qt.Key_Right:
+            self.open_next_osc()
+        else:
+            event.ignore()  # Позволяет обработать событие другим обработчикам
+
+
+    def check_next_prev_osc(self) -> None:
+        if self.osc_now + 1 >= self.num_osc:
+            self.bttn_next_osc.setEnabled(False)
+        elif not self.bttn_next_osc.isEnabled():
+            self.bttn_next_osc.setEnabled(True)
+        if self.osc_now <= self.start_data_osc:
+            self.bttn_prev_osc.setEnabled(False)
+        elif not self.bttn_prev_osc.isEnabled():
+            self.bttn_prev_osc.setEnabled(True)
+
+    def open_next_osc(self) -> None:
+        if self.osc_now >= self.end_data_osc - 1:
+            self.__load_next_osc()
+            self.check_next_prev_osc()
+            if not self.bttn_next_osc.isEnabled():
+                return
+
+        self.osc_now += 1
+        self.now_plot_osc.clear()
+        self.now_plot_spectr.clear()
+
+        self.info_now_num_osc.setText(f"Номер кадра: {self.osc_now + 1} из {self.num_osc}")
+
+        now_ind = self.osc_now - self.start_data_osc
+        self.now_plot_osc_item = self.now_plot_osc.plot(self.osc_datas[now_ind])
+        spectr = four2(np.array(self.osc_datas[now_ind], dtype=np.float64))
+        x = [i / (len(spectr) / 2 / 500) for i in range(round(len(spectr) / 2))]
+        self.now_plot_spectr_item = self.now_plot_spectr.plot(x, spectr[:round(len(spectr) / 2)])
+
+        # Добавляем информацию о том, какя частота более выражена в сигнале
+        index = np.argmax(spectr[:round(len(spectr) / 2)])
+        self.text = pg.TextItem(f"{x[index]} кГц", anchor=(0, 1), color="r")
+        self.now_plot_spectr.addItem(self.text)
+
+        # Добавляем информацию децибелах
+        self.dB_text = pg.TextItem(f"{self.dB_data[now_ind]} Дб", anchor=(0, 1), color="r")
+        self.now_plot_osc.addItem(self.dB_text)
+
+        self.__update_info_position()
+        self.check_next_prev_osc()
+
+        # Отправляем сигнал о том, что номер осциллограммы изменился
+        self.osc_now_changed.emit(self.osc_now)
+
+    def open_prev_osc(self) -> None:
+        if self.osc_now == 0:
+            return
+        
+        if self.osc_now <= (self.start_data_osc - 1):
+            self.__load_prev_osc()
+            self.check_next_prev_osc()
+            if not self.bttn_prev_osc.isEnabled():
+                return
+        self.osc_now -= 1
+        self.now_plot_osc.clear()
+        self.now_plot_spectr.clear()
+
+        self.info_now_num_osc.setText(f"Номер кадра: {self.osc_now + 1} из {self.num_osc}")
+
+        now_ind = self.osc_now - self.start_data_osc
+        self.now_plot_osc_item = self.now_plot_osc.plot(self.osc_datas[now_ind])
+        spectr = four2(np.array(self.osc_datas[now_ind], dtype=np.float64))
+        x = [i / (len(spectr) / 2 / 500) for i in range(round(len(spectr) / 2))]
+        self.now_plot_spectr_item = self.now_plot_spectr.plot(x, spectr[:round(len(spectr) / 2)])
+
+        # Добавляем текст (аннотацию)
+        index = np.argmax(spectr[:round(len(spectr) / 2)])
+        self.text = pg.TextItem(f"{x[index]} кГц", anchor=(0, 1), color="r")
+        self.now_plot_spectr.addItem(self.text)
+
+        # Добавляем информацию децибелах
+        self.dB_text = pg.TextItem(f"{self.dB_data[now_ind]} Дб", anchor=(0, 1), color="r")
+        self.now_plot_osc.addItem(self.dB_text)
+
+        self.__update_info_position()
+        self.check_next_prev_osc()
+
+        # Отправляем сигнал о том, что номер осциллограммы изменился
+        self.osc_now_changed.emit(self.osc_now)
+    
+    def __set_style_for_app(self) -> None:
+        """Устанавливает стиль для приложения."""
+        self.setStyleSheet("""
+            QMainWindow {
+                background-color: #fff;
+            }
+            QLabel {
+                font-size: 13px;
+            }
+            QPushButton {
+                font-size: 12px;
+                padding: 5px;
+            }
+            """)
+
+
+class SeeOSC(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.menu = None
+        self.create_menu()
+
+    def create_menu(self):
+        self.menu = MainMenu()
+
+        self.setCentralWidget(self.menu)
+        self.show()
+
+
+if __name__ == "__main__":
+    try:
+        app = QtWidgets.QApplication([])
+        ex = SeeOSC()
+        sys.exit(app.exec())
+    except Exception as e:
+        QMessageBox.critical(None, "Критическая ошибка", f"Критическая ошибка: {e}")
